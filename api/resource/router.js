ce6e1bbacce064d191c0c99ca91c0fc495ae7005
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2022-03-24 18:57:06 -0700

    wip

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,16 +1,7 @@
 // build your `/api/resources` router here
 const router = require('express').Router();
 
-router.use('*', (req, res) => {
-    res.json({ api: 'up' })
-})
 
-router.use((err, req, res, next) => { // eslint-disable-line
-    res.status(500).json({
-        customMessage: 'something went wrong inside the resource router',
-        message: err.message,
-        stack: err.stack,
-    })
-})
+
 
 module.exports = router
